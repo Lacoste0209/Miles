bdfabd22e9c4ffd9c10f147528242a7580e2ed09
Konstantin Kharun <konstantin19980209@gmail.com>
2022-11-08 16:02:40 +0300

    Miles

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+        public static void main(String[] args) {
+            int amountInRubles = 5_000;
+            int percentInRubles = 20;
+            int bonus = amountInRubles / percentInRubles;
+            System.out.println(bonus);
+            System.out.println( "За использование нашей авиа компании за каждую потраченную милю вы получаете бонус 1 миля = 1 бонусный рубль");
+
+
+
+        }
+}
